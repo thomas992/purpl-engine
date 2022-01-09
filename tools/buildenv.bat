d9111807f79e97e64e1ed0e97f09c017fe9a11dc
MobSlicer152 <brambleclaw1414@gmail.com>
2022-01-09 01:27:04 -0500

    Added debug print statement

--- tools/buildenv.bat
+++ tools/buildenv.bat
@@ -14,6 +14,7 @@ if not exist "%vs2019_install%" (
 for /F "tokens=* USEBACKQ" %%F in (`type "%vs2019_install%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"`) do (
     set msvc_defaultver=%%F
 )
+echo Default MSVC version is %msvc_defaultver%
 set msvc_path=%vs2019_install%\VC\Tools\MSVC\%msvc_defaultver%
 c:
 pushd "%vs2019_install%\VC\Auxiliary\Build"
