25de7f1c70971d44e885e4524d22cec9cdb2caa4
MobSlicer152 <brambleclaw1414@gmail.com>
2022-01-09 01:20:57 -0500

    Adjusted buildenv

--- tools/buildenv.bat
+++ tools/buildenv.bat
@@ -3,13 +3,12 @@
 :: I kinda wanted to call this script Razzle, but that's an honour only Microsoft is worthy of
 
 :: Try to set up a Visual C++ environment (copied from a script on my development drive)
-if "%COMPAT%"=="1" (
-	set "vs2019_install=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
-) else (
-	set "vs2019_install=C:\Program Files\Microsoft Visual Studio\2022\Preview"
-	if not exist "%vs2019_install%" (
-		set "vs2019_install=C:\Program Files\Microsoft Visual Studio\2022\Community"
-	)
+set "vs2019_install=C:\Program Files\Microsoft Visual Studio\2022\Preview"
+if not exist "%vs2019_install%" (
+	set "vs2019_install=C:\Program Files\Microsoft Visual Studio\2022\Community"
+)
+if not exist "%vs2019_install%" (
+	set "vs2019_install=C:\Program Files\Microsoft Visual Studio\2019\Enterprise" :: Github Actions gives enterprise installs
 )
 
 for /F "tokens=* USEBACKQ" %%F in (`type "%vs2019_install%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"`) do (
