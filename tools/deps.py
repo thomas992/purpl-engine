232a4a97a8dc4ac14509e3a36216f49f82009b72
MobSlicer152 <brambleclaw1414@gmail.com>
2022-01-30 16:50:23 -0500

    Damn iPad keyboard

--- tools/deps.py
+++ tools/deps.py
@@ -388,7 +388,7 @@ if not dry_run or (dry_run and debug):
     if platform.system() == "Windows":
         print(f"Generating new bgfx import library for {plat[4:]}")
         bgfx_def = outputs["bgfx"][2][1].replace("dll", "def")
-        subprocess.call(f"{tools_dir}bmkdlldef.bat " + outputs["bgfx"][2][1], shell=True, stdout=sys.stdout)
+        subprocess.call(f"{tools_dir}\\mkdlldef.bat " + outputs["bgfx"][2][1], shell=True, stdout=sys.stdout)
         subprocess.call(f"link /lib /nologo /out:{deps_path}/bin/bgfx.lib /machine:{plat[4:]} /def:{bgfx_def}", shell=True, stdout=sys.stdout)
 
 # Clean up the other files
