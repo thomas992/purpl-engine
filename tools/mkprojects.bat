f5b60aa09c54ffde97ffb1c271db2fd9ed7e4114
MobSlicer152 <brambleclaw1414@gmail.com>
2022-02-16 16:10:21 -0500

    Switched to Meson

--- tools/mkprojects.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-call %~dp0buildenv.bat
-
-gn gen %~dp0..\out\win --export-compile-commands --ide=vs2022
-pause
--- tools/openprojects.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call %~dp0buildenv.bat
-
-devenv %~dp0..\out\win\all.sln
