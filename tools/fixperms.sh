2e2b2b9fc565df050d338316a75ca9d2eb184eb4
MobSlicer152 <brambleclaw1414@gmail.com>
2022-04-19 14:00:19 -0400

    Improve fixperms.sh, add glslc binaries

--- tools/fixperms.sh
+++ tools/fixperms.sh
@@ -3,15 +3,19 @@
 HERE=$(dirname "$0")
 for i in $HERE/*.py $HERE/*.sh $HERE/dos2unix $HERE/ninja;
 do
-    echo Setting executable bit on $i
-    chmod a+x $i
+	echo Setting executable bit on $i
+	chmod a+x $i
 done
 
+DEMO="$HERE/../demo"
 ENGINE="$HERE/../engine"
-for i in $(find $ENGINE -type f);
+for i in $DEMO $ENGINE;
 do
-    echo Unsetting executable bit on $i
-    chmod a-x $i
+	for j in $(find $i -type f);
+	do
+		echo Unsetting executable bit on $j
+		chmod a-x $j
+	done
 done
 
 echo Done
