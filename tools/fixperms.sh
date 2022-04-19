cfaec4c53b83365020be3c0dab261d6189013551
MobSlicer152 <brambleclaw1414@gmail.com>
2022-04-19 14:12:56 -0400

    Fix glslc

--- tools/fixperms.sh
+++ tools/fixperms.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 HERE=$(dirname "$0")
-for i in $HERE/*.py $HERE/*.sh $HERE/dos2unix $HERE/ninja;
+for i in $HERE/*.py $HERE/*.sh $HERE/dos2unix $HERE/glslc $HERE/ninja;
 do
 	echo Setting executable bit on $i
 	chmod a+x $i
