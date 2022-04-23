510e2f74dab6fe52013c437908751355f222ea30
MobSlicer152 <brambleclaw1414@gmail.com>
2022-04-23 13:47:43 -0400

    Fix errors

--- cmake/SDKs.cmake
+++ cmake/SDKs.cmake
@@ -30,43 +30,18 @@ function(get_discord_sdk version)
 	if (${PURPL_PROCESSOR} MATCHES "x86")
 		set(DISCORD_SDK_DIR ${CMAKE_BINARY_DIR}/discord_game_sdk)
 		set(DISCORD_SDK_INCLUDE_DIRS ${DISCORD_SDK_DIR}/c;${DISCORD_SDK_DIR}/discord_game_sdk/cpp PARENT_SCOPE)
+		
+		add_library(discord_game_sdk SHARED IMPORTED)
 		if (WIN32)
-			add_library(discord_game_sdk SHARED IMPORTED)
 			set_target_properties(discord_game_sdk PROPERTIES
 								IMPORTED_IMPLIB ${DISCORD_SDK_DIR}/lib/${PURPL_PROCESSOR}/discord_game_sdk.dll.lib
 								IMPORTED_LOCATION ${DISCORD_SDK_DIR}/lib/${PURPL_PROCESSOR}/discord_game_sdk.dll)
-		else()
-			set(DISCORD_HEADERS ${DISCORD_SDK_DIR}/cpp/achievement_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/activity_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/application_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/core.h
-					    ${DISCORD_SDK_DIR}/cpp/discord.h
-					    ${DISCORD_SDK_DIR}/cpp/event.h
-					    ${DISCORD_SDK_DIR}/cpp/ffi.h
-					    ${DISCORD_SDK_DIR}/cpp/image_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/lobby_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/network_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/overlay_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/relationship_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/storage_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/store_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/user_manager.h
-					    ${DISCORD_SDK_DIR}/cpp/voice_manager.h)
-			set(DISCORD_SOURCES ${DISCORD_SDK_DIR}/cpp/achievement_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/activity_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/application_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/core.cpp
-					    ${DISCORD_SDK_DIR}/cpp/image_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/lobby_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/network_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/overlay_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/relationship_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/storage_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/store_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/types.cpp
-					    ${DISCORD_SDK_DIR}/cpp/user_manager.cpp
-					    ${DISCORD_SDK_DIR}/cpp/voice_manager.cpp)
-			add_library(discord_game_sdk STATIC ${DISCORD_HEADERS} ${DISCORD_SOURCES})
+		elseif (APPLE)
+			set_target_properties(discord_game_sdk PROPERTIES
+								IMPORTED_LOCATION ${DISCORD_SDK_DIR}/lib/${PURPL_PROCESSOR}/discord_game_sdk.dylib)
+		elseif (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+			set_target_properties(discord_game_sdk PROPERTIES
+								IMPORTED_LOCATION ${DISCORD_SDK_DIR}/lib/${PURPL_PROCESSOR}/discord_game_sdk.so)
 		endif()
 	endif()
 endfunction()
@@ -85,22 +60,20 @@ function(get_steamworks_sdk)
 	message(STATUS "Steamworks SDK extracted to ${CMAKE_BINARY_DIR}/steamworks_sdk")
 
 	if (${PURPL_PROCESSOR} STREQUAL "x86_64")
-		set(STEAMWORKS_SDK_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/steamworks_sdk/sdk/public PARENT_SCOPE)
-		if (WIN32)
-			set(STEAMWORKS_SDK_LIBRARY ${CMAKE_BINARY_DIR}/steamworks_sdk/sdk/redistributable_bin/win64/steam_api64.lib)
-			set(STEAMWORKS_SDK_BINARY ${CMAKE_BINARY_DIR}/steamworks_sdk/sdk/redistributable_bin/win64/steam_api64.dll)
-		elseif (APPLE)
-			set(STEAMWORKS_SDK_LIBRARY ${CMAKE_BINARY_DIR}/steamworks_sdk/sdk/redistributable_bin/osx/libsteam_api.dylib)
-			set(STEAMWORKS_SDK_BINARY ${STEAMWORKS_SDK_LIBRARY})
-		elseif (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-			set(STEAMWORKS_SDK_LIBRARY ${CMAKE_BINARY_DIR}/steamworks_sdk/sdk/redistributable_bin/linux64/libsteam_api.so)
-			set(STEAMWORKS_SDK_BINARY ${STEAMWORKS_SDK_LIBRARY})
-		endif()
+		set(STEAMWORKS_SDK_DIR ${CMAKE_BINARY_DIR}/steamworks_sdk/sdk)
+		set(STEAMWORKS_SDK_INCLUDE_DIRS ${STEAMWORKS_SDK_DIR}/public PARENT_SCOPE)
+		
 		add_library(steamworks_sdk SHARED IMPORTED GLOBAL)
-		get_filename_component(STEAMWORKS_SDK_SONAME ${STEAMWORKS_SDK_LIBRARY} NAME)
-		set_target_properties(steamworks_sdk PROPERTIES IMPORTED_LOCATION ${STEAMWORKS_SDK_BINARY})
 		if (WIN32)
-			set_target_properties(steamworks_sdk PROPERTIES IMPORTED_IMPLIB ${STEAMWORKS_SDK_LIBRARY})
+			set_target_properties(steamworks_sdk PROPERTIES
+								IMPORTED_IMPLIB ${STEAMWORKS_SDK_DIR}/redistributable_bin/win64/steam_api64.lib
+								IMPORTED_LOCATION ${STEAMWORKS_SDK_DIR}/redistributable_bin/win64/steam_api64.dll)
+		elseif (APPLE)
+			set_target_properties(steamworks_sdk PROPERTIES
+								IMPORTED_LOCATION ${STEAMWORKS_SDK_DIR}/redistributable_bin/osx/libsteam_api.dylib)
+		elseif (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+			set_target_properties(steamworks_sdk PROPERTIES
+								IMPORTED_LOCATION ${STEAMWORKS_SDK_DIR}/redistributable_bin/linux64/libsteam_api.so)
 		endif()
 	endif()
 endfunction()
