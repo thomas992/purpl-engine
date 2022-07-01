53c830e330c06505089a59f313906764608b817c
MobSlicer152 <brambleclaw1414@gmail.com>
2022-07-01 12:27:32 -0400

    Add window

--- engine/main.c
+++ engine/main.c
@@ -6,10 +6,13 @@
 #include "pack.h"
 
 static SDL_Window *g_wnd;
+static int32_t g_wnd_width;
+static int32_t g_wnd_height;
+static bool g_wnd_visible;
 
 bool engine_init(const char *basedir, const char *gamedir, gameinfo_t *game)
 {
-	PURPL_LOG("Initializing engine\n");
+	PURPL_LOG("Initializing engine for game %s\n", game->title);
 
 	PURPL_LOG("Initializing SDL\n");
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -17,19 +20,48 @@ bool engine_init(const char *basedir, const char *gamedir, gameinfo_t *game)
 		return false;
 	}
 
-	g_wnd = SDL_CreateWindow(game->title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 768, SDL_WINDOW_ALLOW_HIGHDPI);
+	g_wnd_width = 1024;
+	g_wnd_height = 576;
+	g_wnd = SDL_CreateWindow(game->title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, g_wnd_width, g_wnd_height, SDL_WINDOW_ALLOW_HIGHDPI);
+	PURPL_ASSERT(g_wnd);
 
 	return true;
 }
 
-void engine_frame(uint64_t delta)
+bool engine_frame(uint64_t delta)
 {
-	PURPL_LOG("\rDelta: %" PRIu64 "ms\t", delta);
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_WINDOWEVENT && event.window.windowID == SDL_GetWindowID(g_wnd)) {
+			switch (event.window.event) {
+			case SDL_WINDOWEVENT_HIDDEN:
+				PURPL_LOG("Window hidden\n");
+				g_wnd_visible = false;
+				break;
+			case SDL_WINDOWEVENT_EXPOSED:
+				PURPL_LOG("Window exposed\n");
+				g_wnd_visible = true;
+				break;
+			case SDL_WINDOWEVENT_CLOSE:
+				PURPL_LOG("Window closed\n");
+				return false;
+			}
+		}
+	}
+
+	return true;
 }
 
 void engine_shutdown(void)
 {
 	PURPL_LOG("Shutting down\n");
+
+	PURPL_LOG("Destroying window\n");
+	SDL_DestroyWindow(g_wnd);
+
+	PURPL_LOG("Shutting down SDL\n");
+	SDL_Quit();
 }
 
 PURPL_INTERFACE void create_interface(dll_t *dll)
