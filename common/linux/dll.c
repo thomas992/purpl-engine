0205e367f76dcce40f5db6caffc6848f2d168bfe
MobSlicer152 <brambleclaw1414@gmail.com>
2022-07-03 11:27:38 -0400

    Make Linux dll.c's logic match the Windows one

--- common/linux/dll.c
+++ common/linux/dll.c
@@ -4,24 +4,30 @@
 
 #include "common/dll.h"
 
-bool sys_dll_load(dll_t *dll)
+bool sys_dll_load(dll_t *dll, bool engine)
 {
 	dll_create_interface_t create_interface;
 
 	dll->handle = dlopen(dll->path, RTLD_LAZY);
-	PURPL_ASSERT(dll->handle);
-
-	*(void **)(&create_interface) = dlsym(dll->handle, "create_interface");
-	if (!create_interface) {
-		PURPL_LOG("DLL %s is not an engine DLL\n", dll->path);
+	if (!dll->handle) {
+		PURPL_LOG("dlopen(\"%s\", RTLD_LAZY) failed: %s\n", dll->path, dlerror());
 		return false;
 	}
 
-	create_interface(dll);
+	if (engine) {
+		*(void **)(&create_interface) = dlsym(dll->handle, "create_interface");
+		if (!create_interface) {
+			PURPL_LOG("DLL %s is not an engine DLL\n", dll->path);
+			return false;
+		}
+
+		create_interface(dll);
+	}
 	return true;
 }
 
 void sys_dll_unload(dll_t *dll)
 {
-	dlclose(dll->handle);
+	if (dll && dll->handle)
+		dlclose(dll->handle);
 }
